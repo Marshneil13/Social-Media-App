9124ee90e7c8011752af7c13d1e3e8a27ce658d5
Marshneil13 <reachmarshneil@hotmail.com>
2024-02-25 22:59:51 +0530

    Routing Implemented

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,20 @@
+import LeftSideBar from '@/components/shared/LeftSideBar'
+import Topbar from '@/components/shared/Topbar'
+import { Outlet } from 'react-router-dom'
 import React from 'react'
+import Bottombar from '@/components/shared/Bottombar'
 
 const RootLayout = () => {
   return (
-    <div>
-      Root Layout
+    <div className="w-full md:flex">
+      <Topbar/>
+      <LeftSideBar/>
+
+      <section className='flex flex-1 h-full'>
+       <Outlet/>
+      </section>
+
+      <Bottombar/>
     </div>
   )
 }
--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Bottombar = () => {
   return (
     <div>
-      Home
+      
     </div>
   )
 }
 
-export default Home
+export default Bottombar
